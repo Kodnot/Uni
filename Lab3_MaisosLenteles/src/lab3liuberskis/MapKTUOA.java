483cd4f8bf879db2277322260d3d8ff5fbfe67c8
Audrius Liuberskis <audriusliub@gmail.com>
2018-12-20 19:07:09 +0200

    Added open address map impl

---
+++ Lab3_MaisosLenteles/src/lab3liuberskis/MapKTUOA.java
@@ -0,0 +1,295 @@
+package lab3liuberskis;
+
+import java.util.Arrays;
+import laborai.studijosktu.HashType;
+import laborai.studijosktu.MapADT;
+import laborai.studijosktu.MapADTp;
+
+public class MapKTUOA<K, V> implements MapADT<K, V> {
+
+    public static final int DEFAULT_INITIAL_CAPACITY = 16;
+    public static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    public static final HashType DEFAULT_HASH_TYPE = HashType.DIVISION;
+
+    // Maišos lentelė
+    protected Entry<K, V>[] table;
+    // Lentelėje esančių raktas-reikšmė porų kiekis
+    protected int size = 0;
+    // Apkrovimo faktorius
+    protected float loadFactor;
+    // Maišos metodas
+    protected HashType ht;
+    //--------------------------------------------------------------------------
+    //  Maišos lentelės įvertinimo parametrai
+    //--------------------------------------------------------------------------
+    // Maksimalus suformuotos maišos lentelės grandinėlės ilgis
+    protected int maxChainSize = 0;
+    // Permaišymų kiekis
+    protected int rehashesCounter = 0;
+    // Paskutinės patalpintos poros grandinėlės indeksas maišos lentelėje
+    protected int lastUpdatedChain = 0;
+    // Lentelės grandinėlių skaičius     
+    protected int chainsCounter = 0;
+    // Einamas poros indeksas maišos lentelėje
+    protected int index = 0;
+
+    /* Klasėje sukurti 4 perkloti konstruktoriai, nustatantys atskirus maišos 
+     * lentelės parametrus. Jei kuris nors parametras nėra nustatomas - 
+     * priskiriama standartinė reikšmė.
+     */
+    public MapKTUOA() {
+        this(DEFAULT_HASH_TYPE);
+    }
+
+    public MapKTUOA(HashType ht) {
+        this(DEFAULT_INITIAL_CAPACITY, ht);
+    }
+
+    public MapKTUOA(int initialCapacity, HashType ht) {
+        this(initialCapacity, DEFAULT_LOAD_FACTOR, ht);
+    }
+
+    public MapKTUOA(float loadFactor, HashType ht) {
+        this(DEFAULT_INITIAL_CAPACITY, loadFactor, ht);
+    }
+
+    public MapKTUOA(int initialCapacity, float loadFactor, HashType ht) {
+        if (initialCapacity <= 0) {
+            throw new IllegalArgumentException("Illegal initial capacity: " + initialCapacity);
+        }
+
+        if ((loadFactor <= 0.0) || (loadFactor > 1.0)) {
+            throw new IllegalArgumentException("Illegal load factor: " + loadFactor);
+        }
+
+        this.table = new Entry[initialCapacity];
+        this.loadFactor = loadFactor;
+        this.ht = ht;
+    }
+
+    /**
+     * Patikrinama ar atvaizdis yra tuščias.
+     *
+     */
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Grąžinamas atvaizdyje esančių porų kiekis.
+     *
+     * @return Grąžinamas atvaizdyje esančių porų kiekis.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Išvalomas atvaizdis.
+     */
+    @Override
+    public void clear() {
+        Arrays.fill(table, null);
+        size = 0;
+        index = 0;
+        lastUpdatedChain = 0;
+        maxChainSize = 0;
+        rehashesCounter = 0;
+        chainsCounter = 0;
+    }
+
+    /**
+     * Patikrinama ar pora egzistuoja atvaizdyje.
+     *
+     * @param key raktas.
+     * @return Patikrinama ar pora egzistuoja atvaizdyje.
+     */
+    @Override
+    public boolean contains(K key) {
+        return get(key) != null;
+    }
+
+    public boolean containsValue(V value) {
+        for (int i = 0; i < table.length; ++i) {
+            Entry<K, V> cur = table[i];
+            while (cur != null) {
+                if (cur.value.equals(value)) {
+                    return true;
+                }
+                cur = cur.next;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Atvaizdis papildomas nauja pora.
+     *
+     * @param key raktas,
+     * @param value reikšmė.
+     * @return Atvaizdis papildomas nauja pora.
+     */
+    @Override
+    public V put(K key, V value) {
+        if (key == null || value == null) {
+            throw new IllegalArgumentException("Key or value is null in put(Key key, Value value)");
+        }
+        index = hash(key, ht);
+        if (table[index] == null) {
+            chainsCounter++;
+        }
+
+        Entry<K, V> node = getInChain(key, table[index]);
+        if (node == null) {
+            table[index] = new Entry<>(key, value, table[index]);
+            size++;
+
+            if (size > table.length * loadFactor) {
+                rehash(table[index]);
+            } else {
+                lastUpdatedChain = index;
+            }
+        } else {
+            node.value = value;
+            lastUpdatedChain = index;
+        }
+
+        return value;
+    }
+
+    /**
+     * Grąžinama atvaizdžio poros reikšmė.
+     *
+     * @return Grąžinama atvaizdžio poros reikšmė.
+     *
+     * @param key raktas.
+     */
+    @Override
+    public V get(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("Key is null in get(Key key)");
+        }
+
+        index = hash(key, ht);
+        Entry<K, V> node = getInChain(key, table[index]);
+        return (node != null) ? node.value : null;
+    }
+
+    /**
+     * Pora pašalinama iš atvaizdžio.
+     *
+     * @param key Pora pašalinama iš atvaizdžio.
+     * @return key raktas.
+     */
+    @Override
+    public V remove(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("Key is null in remove(Key key)");
+        }
+
+        index = hash(key, ht);
+        Entry<K, V> previous = null;
+        for (Entry<K, V> n = table[index]; n != null; n = n.next) {
+            if ((n.key).equals(key)) {
+                if (previous == null) {
+                    table[index] = n.next;
+                } else {
+                    previous.next = n.next;
+                }
+                size--;
+
+                if (table[index] == null) {
+                    chainsCounter--;
+                }
+                return n.value;
+            }
+            previous = n;
+        }
+        return null;
+    }
+
+    /**
+     * Permaišymas
+     *
+     * @param node
+     */
+    private void rehash(Entry<K, V> node) {
+        MapKTUOA mapKTU
+                = new MapKTUOA(table.length * 2, loadFactor, ht);
+        for (int i = 0; i < table.length; i++) {
+            while (table[i] != null) {
+                if (table[i].equals(node)) {
+                    lastUpdatedChain = i;
+                }
+                mapKTU.put(table[i].key, table[i].value);
+                table[i] = table[i].next;
+            }
+        }
+        table = mapKTU.table;
+        maxChainSize = mapKTU.maxChainSize;
+        chainsCounter = mapKTU.chainsCounter;
+        rehashesCounter++;
+    }
+
+    private int findPosition(K key) {
+        
+    }
+
+    // TODO: Should this really be two-dimensional with this impl?
+    @Override
+    public String[][] toArray() {
+        String[][] result = new String[table.length][];
+        int count = 0;
+        for (Entry<K, V> n : table) {
+            result[count++] = new String[]{
+                n.toString()
+            };
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (Entry<K, V> node : table) {
+            if (node != null) {
+                result.append(node.toString()).append(System.lineSeparator());
+            }
+        }
+        return result.toString();
+    }
+
+    public int getEmptyElementCount() {
+        int count = 0;
+        for (int i = 0; i < table.length; ++i) {
+            if (table[i] == null) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    protected class Entry<K, V> {
+
+        // Raktas        
+        protected K key;
+        // Reikšmė
+        protected V value;
+
+        protected Entry() {
+        }
+
+        protected Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return key + "=" + value;
+        }
+    }
+}
